193f81250ad87053f0233287f8dc92ffa13e54a5
Mercy <mercernarymage@gmail.com>
2026-08-03 23:04:40 -0700

    aaa

---
+++ Assets/Level Editor/SwatchScriptableObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Swatch", order = 1)]
+public class SwatchScriptableObject : ScriptableObject
+{
+    public Sprite image;
+    public GameObject prefab0;
+	public GameObject prefab1;
+	public GameObject prefab2;
+}
